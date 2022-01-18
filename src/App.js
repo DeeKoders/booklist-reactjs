47744654ab1681e39235f2b92b10b954f8b85f5d
DeeKoders <deekoders@gmail.com>
2022-01-18 15:27:00 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
 
 function App() {
+
+//setting up array
+const bookShelf = [
+{
+  id:1,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL604_SR604,400_.jpg',
+  title: 'It Ends with Us: A Novel',
+  author: 'Colleen Hoover',
+
+}
+,
+{
+  id:2,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/61+nBGbGW7L._AC_UL604_SR604,400_.jpg',
+  title: 'Verity',
+  author: 'Colleen Hoover',
+
+}
+,
+{
+  id:3,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81gtzoeueoS._AC_UL604_SR604,400_.jpg',
+  title: 'My Little Golden Book About Betty White',
+  author: 'Deborah Hopkinson',
+
+}
+]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='booklist'>
+      {bookShelf.map((book)=>{
+        return(
+          <Books key={book.id} {...book}/>
+        );
+      })}
+    </section>
   );
+}
+
+const Books = (props) =>{
+  const {img,title,author} = props
+  return(
+    <article className='book'>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  )
+
 }
 
 export default App;
