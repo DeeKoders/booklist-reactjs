4ad168a6f55efb7c6a98b988c801d2829449ad20
DeeKoders <deekoders@gmail.com>
2022-01-25 16:26:28 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,57 +1,9 @@
 import './index.css'
-
+import { bookShelf } from './book';
 function App() {
 
 //setting up array
-const bookShelf = [
-{
-  id:1,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL604_SR604,400_.jpg',
-  title: 'It Ends with Us: A Novel',
-  author: 'Colleen Hoover',
 
-}
-,
-{
-  id:2,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/61+nBGbGW7L._AC_UL604_SR604,400_.jpg',
-  title: 'Verity',
-  author: 'Colleen Hoover',
-
-}
-,
-{
-  id:3,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81gtzoeueoS._AC_UL604_SR604,400_.jpg',
-  title: 'My Little Golden Book About Betty White',
-  author: 'Deborah Hopkinson',
-
-}
-,
-{
-  id:4,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/613KCs7szvL._AC_UL604_SR604,400_.jpg',
-  title: 'Ugly Love: A Novel',
-  author: 'Colleen Hoover',
-
-}
-,
-{
-  id:5,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51SSq9F8oPL._AC_UL604_SR604,400_.jpg',
-  title: "Little Blue Truck's Valentine",
-  author: 'Alice Schertle',
-
-}
-,
-{
-  id:6,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51G2Y+mbUpL._AC_UL604_SR604,400_.jpg',
-  title: 'The Last Thing He Told Me: A Novel',
-  author: 'Laura Dave',
-
-}
-]
   return (
     <section className='booklist'>
       {bookShelf.map((book)=>{
